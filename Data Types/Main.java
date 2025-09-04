7390f317d23a5d5a664d801a8228d7f51ac701e1
aniketjadhav77 <aniketomjadhav@gmail.com>
2025-09-04 22:40:18 +0530

    Data Types Examples

---
+++ Data Types/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+    public static void main(String[] args) {
+        
+        int A = 10;
+        double B = 99.99;
+        float C = 7.76f;
+        char D ='A';
+        String E = "aniket how are you ";
+        boolean F = true;
+        long G = 9075186499l;
+        short H = 6874;
+        byte I = 120;
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+        System.out.println(E);
+        System.out.println(F);
+        System.out.println(G);
+        System.out.println(H);
+        System.out.println(I);
+
+    } 
+}
